b37e0f9394b6ae94c1eae68e41705cdb7b35c6e2
Mumo Carlos <mumo.crls@gmail.com>
2012-01-05 17:25:06 -0600

    add layout app and admin layout

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,3 +1,4 @@
 class Admin::ApplicationController < ApplicationController
   protect_from_forgery
+  http_basic_authenticate_with name: "frodo", password: "bagins"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Colegio::Application.routes.draw do
+  root to: 'events#index'
   
+  resources :events 
   namespace :admin do
     root to: 'events#index'
     resources :events
