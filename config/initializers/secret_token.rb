0802ddd800e279018c0c7a9cf37ceba410e6e02b
Mumo Carlos <mumo.crls@gmail.com>
2012-01-05 11:14:10 -0600

    new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Colegio::Application.config.secret_token = '2dc2a70b3d24b7ce669879659227b86c620882679990f9edfcccd54a98d7b0a8e964ad76b154eb2a0302532c3da89b0ddce4ac30509b04fbab9a8daab23b6af3'
