aac8121b5fe7dd613c7275b0df08805eaa9e269c
Mumo Carlos <mumo.crls@gmail.com>
2012-01-08 22:12:08 -0600

    case mysql selector

--- app/models/event.rb
+++ app/models/event.rb
@@ -14,6 +14,15 @@ class Event < ActiveRecord::Base
   def self.small data
     start_date = Time.at(data[:start].to_i).to_date
     end_date = Time.at(data[:end].to_i).to_date
-    where(event_date: start_date..end_date).select('DISTINCT ON (events.event_date) events.title, events.description, events.coordinator, events.event_type, events.place, events.schedule, events.google_map, events.event_date')
+    dates = where(event_date: start_date..end_date)
+    if self.adapter == 'mysql'
+      dates = dates.select('DISTINCT ON (events.event_date) events.title, events.description, events.coordinator, events.event_type, events.place, events.schedule, events.google_map, events.event_date')
+    else
+      dates = dates.group(:event_date)
+    end
+  end
+
+  def self.adapter
+    connection.adapter_name.downcase.to_sym
   end
 end
