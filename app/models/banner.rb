26d267c25fa63a33013d7f4fe1914a931fe5d6a4
Mumo Carlos <mumo.crls@gmail.com>
2012-01-09 23:23:14 -0600

    fix nil images

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -3,14 +3,17 @@ class Banner < ActiveRecord::Base
   accepts_nested_attributes_for :images, :reject_if => :all_blank, :allow_destroy => true
 
   def self.informativo_images
-    self.where(banner_type: 1).first.images
+    banner = self.where(banner_type: 1).first
+    banner.images unless banner.nil?
   end
 
   def self.comercial_images
-    self.where(banner_type: 1).first.images
+    banner = self.where(banner_type: 2).first
+    banner.images unless banner.nil?
   end
 
   def self.sidebar_images
-    self.where(banner_type: 1).first.images
+    self.where(banner_type: 3).first
+    banner.images unless banner.nil?
   end
 end
