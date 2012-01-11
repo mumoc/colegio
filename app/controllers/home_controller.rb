91eb62057bf2c842167f4a1b909262dcae29e371
Mumo Carlos <mumo.crls@gmail.com>
2012-01-11 00:36:44 -0600

    remove download link, change main slider size

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,4 @@ class HomeController < ApplicationController
     @informativo = Banner.informativo_images
     @comercial = Banner.comercial_images    
   end
-
-    
-  def download 
-    send_file 'http://s3.amazonaws.com/mumo-cmoj/zips/pdf.zip', :type=>"application/zip", :disposition => 'attachment'
-  end
-
 end
