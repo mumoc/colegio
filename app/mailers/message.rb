2389f7eef7bab1ce9c2c70a321bd868311c61d34
Mumo Carlos <mumo.crls@gmail.com>
2012-01-15 23:23:07 -0600

    send to mumos

--- app/mailers/message.rb
+++ app/mailers/message.rb
@@ -1,14 +1,14 @@
 class Message < ActionMailer::Base
-  default from: "from@example.com"
+  default from: " CMOJ <from@example.com>"
 
   def contact_mail sender, message
-    mail(:to => 'mumo.crls@gmail.com', :subject => "Test Contact") do |format|
+    mail(:to => ['carlos@mumo.mx','jose@mumo.mx'], :subject => "Test Contact") do |format|
       format.html { render 'public_message', :locals => {:sender => sender, :message => message} }
     end
   end
 
   def suggestion sender, message
-    mail(:to => 'mumo.crls@gmail.com', :subject => "Test Suggestion") do |format|
+    mail(:to => ['carlos@mumo.mx','jose@mumo.mx'], :subject => "Test Suggestion") do |format|
       format.html { render 'suggestions', :locals => {:sender => sender, :message => message} }
     end
   end
