bccf9de50e1046abb326396104a93daecf0dc7c8
Mumo Carlos <mumo.crls@gmail.com>
2012-01-16 21:58:48 -0600

    active class on current controller or current static page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,5 +6,9 @@ module ApplicationHelper
   def banner dimension, image
    image.banner_image.thumb(dimension).url
   end
-
+ 
+  def active_class name
+    active = params[:id].nil? ? current(name) : name.match(params[:id]) 
+    'active' if active
+  end
 end
