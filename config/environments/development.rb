46bc5ff84a6b2e45b68c49eba5509d1637f98d55
Mumo Carlos <mumo.crls@gmail.com>
2012-01-24 21:53:03 -0600

    override ui headers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,6 +24,7 @@ Colegio::Application.configure do
 
   # Do not compress assets
   config.assets.compress = false
+  config.serve_static_assets = false
 
   # Expands the lines which load the assets
   config.assets.debug = true
