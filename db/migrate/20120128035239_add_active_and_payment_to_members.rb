0cce4e6afec379cff6474f1e3d80411922fa5972
Mumo Carlos <mumo.crls@gmail.com>
2012-01-27 22:51:38 -0600

    false or true for booleans

--- db/migrate/20120128035239_add_active_and_payment_to_members.rb
+++ db/migrate/20120128035239_add_active_and_payment_to_members.rb
@@ -1,6 +1,6 @@
 class AddActiveAndPaymentToMembers < ActiveRecord::Migration
   def change
-    add_column :members, :active, :boolean, default: 1
-    add_column :members, :payment, :boolean, default: 1
+    add_column :members, :active, :boolean, default: true
+    add_column :members, :payment, :boolean, default: true
   end
 end
