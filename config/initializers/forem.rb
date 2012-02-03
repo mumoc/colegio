c9178011cd0fd9ddbc912eac1684946fbeae9155
Mumo Carlos <mumo.crls@gmail.com>
2012-02-02 22:25:39 -0600

    added theme to forem

--- config/initializers/forem.rb
+++ config/initializers/forem.rb
@@ -1,2 +1,13 @@
 Forem.user_class = 'Member'
 Forem.email_from_address = "please-change-me@example.com"
+
+require 'forem'
+module Forem
+  module Theme
+    module Base
+      class Engine < Rails::Engine
+        Forem.theme = :base
+      end
+    end
+  end
+end
